6aa8e9b7f38e38a5f4fbb48351cfa4a6b01bc83c
David Freiberger <davidfreiberger@gmail.com>
2020-03-24 12:19:10 -0700

    Address issues in https://github.com/tcunit/TcUnit/pull/91 (#1)
    * Fix tabs and comment
    
    * Remove "Cf" prefix associated with Codesys library
    
    * Align variable naming with library conventions
    
    * Rename xUnit-params so that it's clear that they relate to the xUnit reporting and not to the TcUnit framework in general.
    
    * Fix param not updated in refactor
    
    * Revert change to tsproj file.
    
    * Revert change to tsproj realtime settings
    
    * Add TestStreamBuffer tests to Verifier
    
    * Create independent tests
    
    * Add FileControl tests to Verifier
    
    * Add FileControl test to project
    
    * Added tests for XmlControl to Verifier
    
    * Fixed issue in test
    
    * Change XML structure to test another scenario
    
    * Remove line ids
    
    Co-authored-by: David Freiberger <david.freiberger@cellares.com>

---
+++ TcUnit-Verifier/TcUnit-Verifier_DotNet/TcUnit-Verifier/FB_TestFileControl.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcUnit.Verifier
+{
+    class FB_TestFileControl : TestFunctionBlockAssert
+    {
+        public FB_TestFileControl(IEnumerable<ErrorList.Error> errors, string testFunctionBlockInstance = null)
+            : base(errors, testFunctionBlockInstance)
+        {
+            Test_Read();
+            Test_Open();
+            Test_Write();
+            Test_Close();
+            Test_Delete();
+        }
+
+        private void Test_Read()
+        {
+            string testMessage = "PRG_TEST." + _testFunctionBlockInstance + "@Test_Read";
+            AssertDoesNotContainMessage(testMessage, EnvDTE80.vsBuildErrorLevel.vsBuildErrorLevelHigh);
+        }
+  
+        private void Test_Open()
+        {
+            string testMessage = "PRG_TEST." + _testFunctionBlockInstance + "@Test_Open";
+            AssertDoesNotContainMessage(testMessage, EnvDTE80.vsBuildErrorLevel.vsBuildErrorLevelHigh);
+        }
+
+        private void Test_Write()
+        {
+            string testMessage = "PRG_TEST." + _testFunctionBlockInstance + "@Test_Write";
+            AssertDoesNotContainMessage(testMessage, EnvDTE80.vsBuildErrorLevel.vsBuildErrorLevelHigh);
+        }
+
+        private void Test_Close()
+        {
+            string testMessage = "PRG_TEST." + _testFunctionBlockInstance + "@Test_Close";
+            AssertDoesNotContainMessage(testMessage, EnvDTE80.vsBuildErrorLevel.vsBuildErrorLevelHigh);
+        }
+
+        private void Test_Delete()
+        {
+            string testMessage = "PRG_TEST." + _testFunctionBlockInstance + "@Test_Delete";
+            AssertDoesNotContainMessage(testMessage, EnvDTE80.vsBuildErrorLevel.vsBuildErrorLevelHigh);
+        }
+    }
+}
---
+++ TcUnit-Verifier/TcUnit-Verifier_DotNet/TcUnit-Verifier/FB_TestStreamBuffer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcUnit.Verifier
+{
+    class FB_TestStreamBuffer : TestFunctionBlockAssert
+    {
+        public FB_TestStreamBuffer(IEnumerable<ErrorList.Error> errors, string testFunctionBlockInstance = null)
+            : base(errors, testFunctionBlockInstance)
+        {
+            Test_BufferSizeDiffers();
+            Test_Length();
+            Test_Append();
+            Test_Clear();
+            Test_Find();
+            Test_Copy();
+            Test_CutOff();
+        }
+
+        private void Test_BufferSizeDiffers()
+        {
+            string testMessage = "PRG_TEST." + _testFunctionBlockInstance + "@Test_BufferSizeDiffers";
+            AssertDoesNotContainMessage(testMessage, EnvDTE80.vsBuildErrorLevel.vsBuildErrorLevelHigh);
+        }
+
+        private void Test_Append()
+        {
+            string testMessage = "PRG_TEST." + _testFunctionBlockInstance + "@Test_Append";
+            AssertDoesNotContainMessage(testMessage, EnvDTE80.vsBuildErrorLevel.vsBuildErrorLevelHigh);
+        }
+
+        private void Test_Clear()
+        {
+            string testMessage = "PRG_TEST." + _testFunctionBlockInstance + "@Test_Clear";
+            AssertDoesNotContainMessage(testMessage, EnvDTE80.vsBuildErrorLevel.vsBuildErrorLevelHigh);
+        }
+
+        private void Test_CutOff()
+        {
+            string testMessage = "PRG_TEST." + _testFunctionBlockInstance + "@Test_CutOff";
+            AssertDoesNotContainMessage(testMessage, EnvDTE80.vsBuildErrorLevel.vsBuildErrorLevelHigh);
+        }
+
+        private void Test_Copy()
+        {
+            string testMessage = "PRG_TEST." + _testFunctionBlockInstance + "@Test_Copy";
+            AssertDoesNotContainMessage(testMessage, EnvDTE80.vsBuildErrorLevel.vsBuildErrorLevelHigh);
+        }
+
+        private void Test_Find()
+        {
+            string testMessage = "PRG_TEST." + _testFunctionBlockInstance + "@Test_Find";
+            AssertDoesNotContainMessage(testMessage, EnvDTE80.vsBuildErrorLevel.vsBuildErrorLevelHigh);
+        }
+
+        private void Test_Length()
+        {
+            string testMessage = "PRG_TEST." + _testFunctionBlockInstance + "@Test_Length";
+            AssertDoesNotContainMessage(testMessage, EnvDTE80.vsBuildErrorLevel.vsBuildErrorLevelHigh);
+        }
+    }
+}
