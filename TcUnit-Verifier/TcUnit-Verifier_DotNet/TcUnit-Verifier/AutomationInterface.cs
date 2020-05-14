ace8eddd44a1415b4158e6da2c48a597df8c5573
Roald Ruiter <roaldruiter@gmail.com>
2020-05-14 21:43:30 +0200

    Removed unused imports, a unused method and some white lines.

--- TcUnit-Verifier/TcUnit-Verifier_DotNet/TcUnit-Verifier/AutomationInterface.cs
+++ TcUnit-Verifier/TcUnit-Verifier_DotNet/TcUnit-Verifier/AutomationInterface.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TCatSysManagerLib;
+﻿using TCatSysManagerLib;
 
 namespace TcUnit.Verifier
 {
@@ -18,7 +13,6 @@ namespace TcUnit.Verifier
         private ITcConfigManager configManager = null;
         private ITcSmTreeItem plcTreeItem = null;
         private ITcSmTreeItem routesTreeItem = null;
-
 
         public AutomationInterface(EnvDTE.Project project)
         {
--- TcUnit-Verifier/TcUnit-Verifier_DotNet/TcUnit-Verifier/TestFunctionBlockAssert.cs
+++ TcUnit-Verifier/TcUnit-Verifier_DotNet/TcUnit-Verifier/TestFunctionBlockAssert.cs
@@ -1,11 +1,7 @@
 ﻿using EnvDTE80;
 using log4net;
-using log4net.Config;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TcUnit.Verifier
 {
